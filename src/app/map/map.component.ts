d0b54e6c805c0f214177412372380559bad652ea
Daria Bednarz <dariab12345@gmail.com>
2022-01-05 21:39:16 +0100

    :sparkles: correct red markers on map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,7 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
-import {DataService, Station} from "../data.service";
-import {Observable} from "rxjs";
+import {DataService} from "../data.service";
 
 @Component({
   selector: 'app-map',
@@ -13,14 +12,14 @@ export class MapComponent implements AfterViewInit {
   private map: any;
   private points: any;
 
-  // private addMarker(code: any, lat: any, lng: any): void {
-  //   var c = L.circle([lat,lng], {
-  //     color: 'red',
-  //     fillColor: '#f03',
-  //     fillOpacity: 0.5,
-  //     radius: 0
-  //   }).addTo(this.map);
-  // }
+  private addMarker(code: any, lat: any, lng: any): void {
+    L.circle([lat,lng], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 100
+    }).bindPopup(code).openPopup().addTo(this.map);
+  }
 
   private initMap(): void {
     this.map = L.map('map', {
@@ -28,27 +27,11 @@ export class MapComponent implements AfterViewInit {
       zoom: 3,
       preferCanvas: true
     });
-    let points = [
-      ["Szczecin", 53.428543, 14.552812],
-      ["Gdańsk", 54.372158, 18.638306],
-      ["Kraków", 50.049683, 19.944544]
-    ];
-    // console.log(this.points);
-    console.log(this.points);
-    console.log(points);
-    console.log(typeof(this.points));
-    console.log(typeof(points));
+
     this.points.forEach((point: any[]) => {
-      console.log(point);
-      L.marker([point[1], point[2]], {
-      }).addTo(this.map);//.bindPopup('marker ' + i);
-      // L.circle([point[1],point[2]], {
-      //   color: 'red',
-      //   fillColor: '#f03',
-      //   fillOpacity: 0.5,
-      //   radius: 0
-      // }).addTo(this.map);
-      // this.addMarker(point[0], point[1], point[2]);
+      point.forEach((station: any) => {
+        this.addMarker(station.name, station.gegrLat, station.gegrLon);
+      })
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -60,12 +43,7 @@ export class MapComponent implements AfterViewInit {
   }
 
   constructor(service: DataService) {
-    this.points = [];
-    service.getStations().subscribe(data => {
-      data.forEach(point => {
-        this.points.push([point.city.name, Number(point.gegrLat), Number(point.gegrLon)]);
-      });
-    });
+    this.points = service.getStations();
   }
 
   ngAfterViewInit(): void {
