6a0c66c81a4380ca74c20ab2f9d7d54264bc4df5
Paweł <kalicki.paul@icloud.com>
2022-01-05 20:57:08 +0100

    :sparkles: Change data source with markers

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -13,38 +13,43 @@ export class MapComponent implements AfterViewInit {
   private map: any;
   private points: any;
 
-  private greenIcon = L.icon({ //add this new icon
-    iconUrl: 'https://img.icons8.com/plumpy/344/sensor.png',
-    iconSize: [38, 38], // size of the icon
-  });
-
-  private addMarker(code: any, lat: any, lng: any): void {
-    var p = L.marker([lat,lng]).setIcon(this.greenIcon);
-    p.bindPopup(code);
-    p.addTo(this.map);
-    var c = L.circle([lat,lng], {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.5,
-      radius: 0
-    }).addTo(this.map);
-  }
+  // private addMarker(code: any, lat: any, lng: any): void {
+  //   var c = L.circle([lat,lng], {
+  //     color: 'red',
+  //     fillColor: '#f03',
+  //     fillOpacity: 0.5,
+  //     radius: 0
+  //   }).addTo(this.map);
+  // }
 
   private initMap(): void {
     this.map = L.map('map', {
       center: [52.237049, 21.017532],
-      zoom: 3
+      zoom: 3,
+      preferCanvas: true
     });
-
     let points = [
       ["Szczecin", 53.428543, 14.552812],
       ["Gdańsk", 54.372158, 18.638306],
       ["Kraków", 50.049683, 19.944544]
     ];
-
-    for (let i = 0; i < points.length; i++) {
-      this.addMarker(points[i][0], points[i][1], points[i][2]);
-    }
+    // console.log(this.points);
+    console.log(this.points);
+    console.log(points);
+    console.log(typeof(this.points));
+    console.log(typeof(points));
+    this.points.forEach((point: any[]) => {
+      console.log(point);
+      L.marker([point[1], point[2]], {
+      }).addTo(this.map);//.bindPopup('marker ' + i);
+      // L.circle([point[1],point[2]], {
+      //   color: 'red',
+      //   fillColor: '#f03',
+      //   fillOpacity: 0.5,
+      //   radius: 0
+      // }).addTo(this.map);
+      // this.addMarker(point[0], point[1], point[2]);
+    });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
@@ -55,8 +60,11 @@ export class MapComponent implements AfterViewInit {
   }
 
   constructor(service: DataService) {
-    this.points = service.getStations().subscribe(data => {
-      console.log(data);
+    this.points = [];
+    service.getStations().subscribe(data => {
+      data.forEach(point => {
+        this.points.push([point.city.name, Number(point.gegrLat), Number(point.gegrLon)]);
+      });
     });
   }
 
